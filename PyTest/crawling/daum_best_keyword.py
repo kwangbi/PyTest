c94d9ad8aa17fa1c76f4c911c9ea785710e3f6f5
p042360 <p042360@SKT-INT8-A039.sktsoc.ad>
2018-06-28 11:09:26 +0900

    1111

--- PyTest/crawling/daum_best_keyword.py
+++ PyTest/crawling/daum_best_keyword.py
@@ -4,23 +4,27 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
+
 while True:
-    res = requests.get('http://www.naver.com')
+
+    res = requests.get('http://www.daum.net')
     assert res.status_code is 200
     
     dom = BeautifulSoup(res.content,"html.parser")
     #print(dom)
-    
-    ranking_elements = dom.select("li.ah_item")
+        
+    ranking_elements = dom.select("div.roll_txt")
     #print(ranking_elements)
     
     for i, ranking_element in enumerate(ranking_elements):
         i = i+1
-        ranking_title_element = ranking_element.select(".ah_k")[0]
-        #print(i+1,"위 : ", ranking_title_element.text)
+        ranking_title_element = ranking_element.select(".link_issue")[0]
         #print(ranking_title_element.text)
         print "[%d] : %s" %(i,ranking_title_element.text)
-
+        
+        if i==10:
+            break
+        
     print("-" * 30)
     print("refresh.....1 min")
     print("-" * 30)
